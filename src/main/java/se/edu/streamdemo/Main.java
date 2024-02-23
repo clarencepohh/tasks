138d315af2693b1e93e07e021ee5887bf377fa93
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2024-02-23 17:15:40 +0800

    New methods in main

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -14,11 +14,14 @@ public class Main {
 
         System.out.println("Printing all data ...");
         printAllData(tasksData);
+        printALlDataUsingStreams(tasksData);
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
+        printDeadlineUsingStream(tasksData);
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines (iteration): " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines (stream): " + countDeadlinesUsingStream(tasksData));
 
     }
 
