11949b136901cb9d6b2c263fed49c2408b6f6bbe
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2024-02-23 17:17:20 +0800

    Implement printAllData using streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -42,6 +42,12 @@ public class Main {
         }
     }
 
+    public static void printALlDataUsingStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using stream...");
+        tasks.stream()
+                .forEach(System.out::println);
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
